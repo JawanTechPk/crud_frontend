38efd9f3945cd6c4ecc655d6e7fd56ed907fd6be
basitahmed1997@gmail.com <farazmohammad190@gmail.com>
2024-03-10 12:42:19 +0500

    auth done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import SignUp from "./pages/Signup";
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./pages/Login";
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Routes>
+        <Route path={"/signup"} element={<SignUp />} />
+        <Route index element={<SignIn />} />
+      </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/constant/index.js
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://impossible-red-sea-lion.cyclic.app/api";
